5b09d79713113f76beff353e4e31a64dece255da
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-05 14:23:03 +0900

    URL 단축 방식 설정

--- src/main/java/com/koreait/surl_project_11/SurlController.java
+++ src/main/java/com/koreait/surl_project_11/SurlController.java
@@ -1,28 +1,53 @@
 package com.koreait.surl_project_11;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class SurlController {
 
-    private List<Surl> surls;
-    private long surlsLastid;
+    private List<Surl> surls = new ArrayList<>();
+    private long surlsLastId;
 
     @GetMapping("/add")
     @ResponseBody
-    public String add(String body, String url) {
+    public Surl add(String body, String url) {
         Surl surl = Surl.builder()
-                .id(++surlsLastid)
+                .id(++surlsLastId)
                 .body(body)
                 .url(url)
                 .build();
 
         surls.add(surl);
-        return surl.toString();
+        return surl;
+    }
+
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public String add(
+            @PathVariable String body,
+            HttpServletRequest req
+    ) {
+        String url = req.getRequestURI();
+
+        if (req.getQueryString() != null) {
+            url += "?" + req.getQueryString();
+        }
+
+        String[] urlBits = url.split("/", 4);
+
+        System.out.println("Arrays.toString(urlBits) : " + Arrays.toString(urlBits));
+
+        url = urlBits[3];
+
+        return url;
     }
 
 }
