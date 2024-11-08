029669be7cc862cebfc6d97f5817d291328bfc9f
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-08 15:26:33 +0900

    Service에 @Transactional 적용

--- src/main/java/com/koreait/surl_project_11/domain/article/article/service/ArticleService.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/service/ArticleService.java
@@ -5,12 +5,16 @@ import com.koreait.surl_project_11.domain.article.article.repository.ArticleRepo
 import com.koreait.surl_project_11.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+// @Transactional : public 메서드에는 붙는게 관례
+// readOnly = true : SELECT만 할거 같을 경우
+@Transactional(readOnly = true)
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
@@ -24,6 +28,7 @@ public class ArticleService {
     // - 게시글 생성에 대한 결과 메세지
     // - 결과 코드
     // - 10분 안에 5개 이상 작성 시 실패하도록
+    @Transactional // (readOnly = true) 아닌경우는 따로 @Transactional 명시해줘야한다.
     public RsData<Article> write(String title, String body) {
         Article article = Article.builder()
                 .title(title)
@@ -38,7 +43,6 @@ public class ArticleService {
     public void delete(Article article) {
         articleRepository.delete(article);
     }
-
 
     public Optional<Article> findById(long id) {
         return articleRepository.findById(id);
--- src/main/java/com/koreait/surl_project_11/domain/member/member/service/MemberService.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/service/MemberService.java
@@ -6,20 +6,25 @@ import com.koreait.surl_project_11.global.exception.GlobalException;
 import com.koreait.surl_project_11.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+// @Transactional : public 메서드에는 붙는게 관례, private 메서드에는 적용되지 않는다.
+// readOnly = true : SELECT만 할거 같을 경우
+@Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Transactional()
     public RsData<Member> join(String username, String password, String nickname) {
-//        ==v1==
-//        boolean present = findByUsername(username).isPresent();
-//        if (present) {
-//            throw new GlobalException("400-1", "이미 존재하는 아이디입니다.");
-//        }
+//      ==v1==
+//      boolean present = findByUsername(username).isPresent();
+//      if (present) {
+//          throw new GlobalException("400-1", "이미 존재하는 아이디입니다.");
+//      }
 //      ==v2==
         findByUsername(username).ifPresent(m -> {
             throw new GlobalException("400-1", "이미 존재하는 아이디입니다.");
