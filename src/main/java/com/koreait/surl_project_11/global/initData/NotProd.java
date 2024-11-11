8f0c550de273408ddea5b026a6dbcbec3aeac632
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-11 16:36:25 +0900

    DB test

--- src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
+++ src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
@@ -1,22 +1,20 @@
 package com.koreait.surl_project_11.global.initData;
 
-import com.koreait.surl_project_11.domain.article.article.entity.Article;
-import com.koreait.surl_project_11.domain.article.article.service.ArticleService;
 import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.transaction.annotation.Transactional;
 
-@Profile("!prod") // !prod == dev(개발 모드) or test(테스트 모드) 일 때 실행하겠다.
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class NotProd {
 
     // this를 통한 객체 내부에서의 메서드 호출은 @Transactional을 작동시키지 않는다.
@@ -27,7 +25,6 @@ public class NotProd {
     @Autowired
     private NotProd self;
 
-    private final ArticleService articleService;
     private final MemberService memberService;
 
     @Bean // 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체 -> 실행될 때 자동으로!
@@ -43,17 +40,18 @@ public class NotProd {
         // 읽기전용 트랜잭션
         // Article 테이블에 데이터가 이미 존재한다면 종료
         // (TRUNCATE 개념 -> 테이블 초기화, AUTO_INCREMENT 적용됐던 것도 삭제.)
-        if (articleService.count() > 0) return;
+        log.debug("initAll started");
+
+        if (memberService.count() > 0) return;
 
         // 쓰기전용 트랜잭션
         Member memberUser1 = memberService.findByUsername("user1").get();
         Member memberUser2 = memberService.findByUsername("user2").get();
 
-        Article article1 = articleService.write(memberUser1, "제목 1", "내용 1").getData();
-        Article article2 = articleService.write(memberUser1, "제목 2", "내용 2").getData();
-
-        Article article3 = articleService.write(memberUser2, "제목 3", "내용 3").getData();
-        Article article4 = articleService.write(memberUser2, "제목 4", "내용 4").getData();
+        Member memberSystem = memberService.join("system", "1234", "시스템").getData();
+        Member memberAdmin = memberService.join("admin", "1234", "관리자").getData();
+        Member member1 = memberService.join("user1", "1234", "회원 1").getData();
+        Member member2 = memberService.join("user2", "1234", "회원 2").getData();
 
     }
 }
