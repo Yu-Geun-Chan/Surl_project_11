6a479513a8df26c867f9059f2ac041d354285f8d
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-15 14:38:53 +0900

    ExceptionHandler를 이용한 예외처리

--- src/main/java/com/koreait/surl_project_11/domain/member/member/controller/MemberController.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/controller/MemberController.java
@@ -1,16 +1,19 @@
 package com.koreait.surl_project_11.domain.member.member.controller;
 
+import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
 import com.koreait.surl_project_11.global.rsData.RsData;
 import com.koreait.surl_project_11.standard.util.Ut;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
@@ -30,6 +33,20 @@ public class MemberController {
         if (Ut.str.isBlank(nickname)) {
             throw new GlobalException("400-3", "nickname을 입력해");
         }
-        return memberService.join(username, password, nickname);
+        try {
+            return  memberService.join(username, password, nickname);
+        } catch (GlobalException e) {
+            e.getRsData().getResultCode().equals("400-1");
+            if (e.getRsData().getResultCode().equals("400-1")) {
+               log.debug("이미 있는 회원입니다.");
+            }
+             return RsData.of("400-C", "커스텀 예외 메세지", Member.builder().build());
+        }
+    }
+
+    @GetMapping("/testThrowIllegalArgumentException")
+    @ResponseBody
+    public RsData<Member> testThrowIllegalArgumentException() {
+        throw new IllegalArgumentException("IllegalArgumentException");
     }
 }
