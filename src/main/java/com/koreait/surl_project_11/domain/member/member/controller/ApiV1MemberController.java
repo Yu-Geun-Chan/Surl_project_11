99a6a7faa1c4a4c4a8851aacdabf6eaf50b509f3
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-18 13:36:34 +0900

    ApiV1MemberController 생성

--- src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
@@ -5,43 +5,42 @@ import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
 import com.koreait.surl_project_11.global.rsData.RsData;
 import com.koreait.surl_project_11.standard.util.Ut;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 @Slf4j
-public class MemberController {
+public class ApiV1MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/join")
-    @ResponseBody
-    public RsData join(
-            String username, String password, String nickname
-    ) {
-        if (Ut.str.isBlank(username)) {
-//            return RsData.of("400-1", "username을 입력해");
+    @AllArgsConstructor
+    @Getter
+    public static class MemberJoinRequestBody {
+        private String username;
+        private String password;
+        private String nickname;
+    }
+
+    // POST api/v1/members/join
+    @PostMapping("") // "join"을 쓰지않고 ""(빈문자열)로 해도 된다. why? -> POST니까
+    public RsData<Member> join(
+            @RequestBody MemberJoinRequestBody requestBody) {
+        if (Ut.str.isBlank(requestBody.username)) {
             throw new GlobalException("400-1", "username을 입력해");
         }
-        if (Ut.str.isBlank(password)) {
+        if (Ut.str.isBlank(requestBody.password)) {
             throw new GlobalException("400-2", "password을 입력해");
         }
-        if (Ut.str.isBlank(nickname)) {
+        if (Ut.str.isBlank(requestBody.nickname)) {
             throw new GlobalException("400-3", "nickname을 입력해");
         }
-        try {
-            return  memberService.join(username, password, nickname);
-        } catch (GlobalException e) {
-            e.getRsData().getResultCode().equals("400-1");
-            if (e.getRsData().getResultCode().equals("400-1")) {
-               log.debug("이미 있는 회원입니다.");
-            }
-             return RsData.of("400-C", "커스텀 예외 메세지", Member.builder().build());
-        }
+        return memberService.join(requestBody.username, requestBody.password, requestBody.nickname);
     }
 
     @GetMapping("/testThrowIllegalArgumentException")
