1ee6b31eeed01f654049949dbe15735748fe1b3e
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-18 15:32:00 +0900

    Aspect, RsData 상태코드를 응답코드로

--- src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/controller/ApiV1MemberController.java
@@ -9,7 +9,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/members")
--- src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
@@ -25,4 +25,7 @@ public class Rq {
     public String getCurrentUrlPath() {
         return req.getRequestURI();
     }
+    public void setStatusCode(int statusCode) {
+        resp.setStatus(statusCode);
+    }
 }
