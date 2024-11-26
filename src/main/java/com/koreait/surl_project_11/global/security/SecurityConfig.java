d96c989dea20567107d2ff0e4373c354b54dd91c
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-26 15:09:53 +0900

    기본 설정에서 특정 URL만 허용

---
+++ src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.koreait.surl_project_11.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/h2-console/**").permitAll() // permitAll() : 제재하지마
+                                .requestMatchers("/actuator/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin.permitAll()
+                );
+
+        return http.build();
+    }
+}
