9af1a3d0b13a4731427f06856378deafd2cbcd1f
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-26 15:27:39 +0900

    비밀번호 암호화

--- src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
@@ -9,6 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // 보안성이 매우 뛰어나다.
     }
 }
