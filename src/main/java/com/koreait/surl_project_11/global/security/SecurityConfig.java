008be74c3a688927447f1cdf633c46697a9d10b0
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-26 15:55:15 +0900

    CustomAuthenticationFilter 추가

--- src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.koreait.surl_project_11.global.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -15,9 +22,8 @@ public class SecurityConfig {
                         authorizeRequests
                                 .requestMatchers("/h2-console/**").permitAll() // permitAll() : 제재하지마
                                 .requestMatchers("/actuator/**").permitAll()
-                                .anyRequest().authenticated()
+                                .anyRequest().authenticated() // 위에 2개가 아니라면 로그인을 검사해
                 )
-                // h2-console을 위한
                 .headers(
                         headers ->
                                 headers.frameOptions(
@@ -31,7 +37,7 @@ public class SecurityConfig {
                 ) // 타임리프, MPA에서는 csrf를 사용한다. 하지만 REST API 방식은 csrf를 끈다.
                 .formLogin(formLogin ->
                         formLogin.permitAll()
-                );
+                ).addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
