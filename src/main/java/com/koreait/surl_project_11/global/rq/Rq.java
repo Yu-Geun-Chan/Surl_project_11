d964e4e476df7ac068c9b40d4a4119837245f382
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-26 13:46:43 +0900

    쿠키

--- src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
@@ -4,11 +4,14 @@ import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
 import com.koreait.surl_project_11.standard.util.Ut;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Arrays;
 
 @Component
 @RequestScope
@@ -17,26 +20,25 @@ public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
     private final MemberService memberService;
+
     private Member member;
 
-//    @Getter
-//    @Setter
-//    private Member member;
+    //   @Getter
+    //   @Setter
+    //  private Member member;
 
     public Member getMember() {
-        // 프록시 객체를 리턴한다. (객체를 만드는데 SQL이 작동하지 않아)
-        // 프록시 : 효율적
-        if(member != null) return member; // 캐시 데이터 방식 == 메모리 캐싱
+        if (member != null) return member; // 메모리 캐싱
 
-        String actorUsername = req.getParameter("actorUsername");
-        String actorPassword = req.getParameter("actorPassword");
+        String actorUsername = getCookieValue("actorUsername", null);
+        String actorPassword = getCookieValue("actorPassword", null);
 
-//        if(actorUsername == null) actorUsername = req.getHeader("actorUsername");
-//        if(actorPassword == null) actorPassword = req.getHeader("actorPassword");
+//        String actorUsername = req.getParameter("actorUsername");
+//        String actorPassword = req.getParameter("actorPassword");
 
-        if(actorUsername == null || actorPassword == null) {
+        if (actorUsername == null || actorPassword == null) {
             String authorization = req.getHeader("Authorization");
-            if(authorization != null) {
+            if (authorization != null) {
                 authorization = authorization.substring("bearer ".length());
                 String[] authorizationBits = authorization.split(" ", 2);
                 actorUsername = authorizationBits[0];
@@ -44,18 +46,32 @@ public class Rq {
             }
         }
 
+//        if (actorUsername == null) actorUsername = req.getHeader("actorUsername");
+//        if (actorPassword == null) actorPassword = req.getHeader("actorPassword");
+
         if (Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디) 입력해줘");
         if (Ut.str.isBlank(actorPassword)) throw new GlobalException("401-2", "인증정보(비밀번호) 입력해줘");
 
-        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("403-3", "해당 회원은 존재하지 않습니다."));
-        if(!loginedMember.getPassword().equals(actorPassword)) throw new GlobalException("404-4", "비밀번호가 틀렸습니다.");
+        Member loginedMember = memberService.findByUsername(actorUsername).orElseThrow(() -> new GlobalException("403-3", "해당 회원은 없어"));
+        if (!loginedMember.getPassword().equals(actorPassword)) throw new GlobalException("403-4", "비밀번호 틀림");
+
+        member = loginedMember;
 
         return loginedMember;
+    }
+
+    private String getCookieValue(String cookieName, String defaultValue) {
+        return Arrays.stream(req.getCookies()) // 쿠키 배열을 스트림으로 변환
+                .filter(cookie -> cookie.getName().equals(cookieName))// 쿠키의 이름이 매개변수로 쓰이게
+                .findFirst() // 첫 번째 요소
+                .map(Cookie::getValue) // 존재하면 쿠키 값으로 매핑
+                .orElse(defaultValue); // 없으면 기본 값
     }
 
     public String getCurrentUrlPath() {
         return req.getRequestURI();
     }
+
     public void setStatusCode(int statusCode) {
         resp.setStatus(statusCode);
     }
