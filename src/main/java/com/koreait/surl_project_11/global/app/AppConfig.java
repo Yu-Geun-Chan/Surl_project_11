17b0a503c20962834e914bc43c66aa3d8658cf36
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-27 14:13:33 +0900

    자격없는 접근에 대한 오류

--- src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
@@ -1,5 +1,8 @@
 package com.koreait.surl_project_11.global.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,6 +10,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+
+    @Getter
+    public static ObjectMapper objectMapper;
+
+    // jackson의 Bean을 가져온다.
+    @Autowired
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 보안성이 매우 뛰어나다.
--- src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
+++ src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
@@ -1,13 +1,24 @@
 package com.koreait.surl_project_11.standard.util;
 
+import com.koreait.surl_project_11.global.app.AppConfig;
+import lombok.SneakyThrows;
+
 public class Ut {
     public static class str {
-        public static boolean isBlank (String str) {
+        public static boolean isBlank(String str) {
             return str == null || str.trim().isBlank();
         }
+
         // public static boolean isNotBlank(String str) {}
-        public static boolean hasLength (String str) {
+        public static boolean hasLength(String str) {
             return !isBlank(str);
+        }
+    }
+
+    public static class json {
+        @SneakyThrows
+        public static String toString(Object obj) {
+            return AppConfig.getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj);
         }
     }
 }
