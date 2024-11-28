2f84f908ba0311fd8189a67125ed5699035dbecf
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-28 13:41:38 +0900

    JWT 방식으로 API KEY 생성

--- src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/app/AppConfig.java
@@ -3,6 +3,7 @@ package com.koreait.surl_project_11.global.app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,4 +25,22 @@ public class AppConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 보안성이 매우 뛰어나다.
     }
+
+    @Getter
+    private static String jwtSecretKey;
+
+    @Value("${custom.secret.jwt.secretKey}")
+    public void setJwtSecretKey(String jwtSecretKey) {
+        this.jwtSecretKey = jwtSecretKey;
+    }
+
+    @Getter
+    private static long accessTokenExpirationSec;
+
+    @Value("${custom.accessToken.expirationSec}")
+    public void setJwtSecretKey(long accessTokenExpirationSec) {
+        this.accessTokenExpirationSec = accessTokenExpirationSec;
+    }
+
 }
+
