2e7d47b54f51956fbf657fd99d3ecf8538d2edfd
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-11-28 15:17:32 +0900

    Refresh Token 적용, 주석 추가

--- src/main/java/com/koreait/surl_project_11/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/koreait/surl_project_11/global/security/CustomAuthenticationFilter.java
@@ -51,23 +51,30 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
+
+        // 기존의 accessToken이 만료됐다면 DB에서 refreshToken과 일치하는 회원 한명을 가져와.
         if (!authTokenService.validateToken(accessToken)) {
             Member member = memberService.findByRefreshToken(refreshToken).orElse(null);
 
+            // 일치하는 회원이 없다면 null이니까 그냥 끝내.
             if(member == null) {
                 filterChain.doFilter(req, resp);
                 return;
             }
 
+            // 새로운 accessToken을 만들어서 Cookie에 저장해.
             String newAccessToken = authTokenService.genToken(member, AppConfig.getAccessTokenExpirationSec());
             rq.setCookie("accessToken", newAccessToken);
             log.debug("accessToken renewed : {}", newAccessToken);
 
+            // 그러고 새로 만들어진 accessToken 값을 기존의 accessToken이 담겨있던 변수에 덮어씌워.
             accessToken = newAccessToken;
         }
 
         Map<String,Object> accessTokenData = authTokenService.getDataFrom(accessToken);
 
+        // 해당하는 회원의 id를 가져와서 id라는 변수에 저장해.
+        // accessTokenData.get("id")는 Integer 타입이니까 형변환을 해야해.
         long id = (int) accessTokenData.get("id");
 
         // 여기까지 왔다는건 인증을 뚫었다는 것.
