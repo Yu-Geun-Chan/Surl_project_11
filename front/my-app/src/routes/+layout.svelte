91dcfa923cc9e00fed0963e6a3fddfc82aa7a35b
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-12-02 15:29:30 +0900

    레이아웃

--- front/my-app/src/routes/+layout.svelte
+++ front/my-app/src/routes/+layout.svelte
@@ -1,6 +1,15 @@
-<script lang="ts">
-	import '../app.css';
-	let { children } = $props();
-</script>
+<header>
+	<nav>
+		<a href="/">메인</a>
+		<a href="/about">설명</a>
+	</nav>
+</header>
 
-{@render children()}
+<!-- 원래 페이지에 있는 요소들이 들어갈 자리 -->
+<slot></slot>
+
+<footer>
+	<nav>
+		<a href="/home/links">링크들</a>
+	</nav>
+</footer>
