8e92f90b3e6446d418b41d3e2a511c2b07b3f8c7
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-12-02 15:53:46 +0900

    로그인

--- front/my-app/src/routes/+layout.svelte
+++ front/my-app/src/routes/+layout.svelte
@@ -1,15 +1,9 @@
 <header>
 	<nav>
 		<a href="/">메인</a>
-		<a href="/about">설명</a>
+		<a href="/member/login">로그인</a>
 	</nav>
 </header>
 
 <!-- 원래 페이지에 있는 요소들이 들어갈 자리 -->
 <slot></slot>
-
-<footer>
-	<nav>
-		<a href="/home/links">링크들</a>
-	</nav>
-</footer>
