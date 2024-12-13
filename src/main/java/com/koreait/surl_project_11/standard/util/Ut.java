873e4f8f821f81d6c8ed6e6a0d7655f5fb8840cc
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-12-13 10:27:09 +0900

    백엔드 통신용 타입스크립트 자동생성

--- src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
+++ src/main/java/com/koreait/surl_project_11/standard/util/Ut.java
@@ -3,23 +3,41 @@ package com.koreait.surl_project_11.standard.util;
 import com.koreait.surl_project_11.global.app.AppConfig;
 import lombok.SneakyThrows;
 
+import java.util.concurrent.TimeUnit;
+
 public class Ut {
     public static class str {
         public static boolean isBlank(String str) {
-            return str == null || str.trim().isBlank();
+            return str == null || str.isBlank();
         }
 
-        // public static boolean isNotBlank(String str) {}
+        //public static boolean isNotBlank(String str) {}
         public static boolean hasLength(String str) {
             return !isBlank(str);
         }
     }
 
     public static class json {
-        // 예외상황 있으면 알아서 처리해 -> 권장하진 않는다.(lombok 공식사이트에서도 권장하지 않는다고 함)
         @SneakyThrows
         public static String toString(Object obj) {
             return AppConfig.getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        }
+    }
+
+    public static class cmd {
+        public static void runAsync(String cmd) {
+            new Thread(() -> {
+                run(cmd);
+            }).start();
+        }
+        public static void run(String cmd) {
+            try {
+                ProcessBuilder processBuilder = new ProcessBuilder(cmd.split(" "));
+                Process process = processBuilder.start();
+                process.waitFor(1, TimeUnit.MINUTES);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
